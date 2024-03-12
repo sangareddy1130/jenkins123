5dfcff1d6fece1c129cb190d65ff2a9d4722f34f
sangareddy <sangareddy1130@gmail.com>
2024-03-12 19:36:48 +0530

    new method added into application file by john

--- Application.java
+++ Application.java
@@ -22,4 +22,25 @@ class Main {
       e.getStackTrace();
     }
   }
+# feauture201 changes by john for ICICI bank project
+public static void main(String[] args) {
+
+    char[] array = new char[100];
+    try {
+      // Creates a reader using the FileReader
+      FileReader input = new FileReader("input.txt");
+
+      // Reads characters
+      input.read(array);
+      System.out.println("Data in the file:");
+      System.out.println(array);
+
+      // Closes the reader
+      input.close();
+    }
+    catch(Exception e) {
+      e.getStackTrace();
+      }
+
+
 }
