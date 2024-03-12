caaf352fe68ffe01e5b8dd16e2ae9324f80b8489
sangareddy <sangareddy1130@gmail.com>
2024-03-12 19:56:13 +0530

    feauture202 changes

--- Application.java
+++ Application.java
@@ -22,4 +22,19 @@ class Main {
       e.getStackTrace();
     }
   }
+#feauture202 changes made by ana on creditcards module
+public static void main(String[] args) {
+
+    // creates a file object
+    File file = new File("file.txt");
+
+    // deletes the file
+    boolean value = file.delete();
+    if(value) {
+      System.out.println("The File is deleted.");
+    }
+    else {
+      System.out.println("The File is not deleted.");
+    }
+  }
 }
